ffedfc851d453f30c8b61056f4b638cdfe4c977f
Sujit Manmode <48320633+sujit1997@users.noreply.github.com>
2024-11-29 10:21:26 +0530

    Update SMPS5Q5.java

--- SMPS5Q5.java
+++ SMPS5Q5.java
@@ -2,17 +2,14 @@ public class SMPS5Q5 {
 
 	public static void main(String[] args){
 
-	int sum = 0;
+	 int sum = 0;
+        int num= 5;
 
-	int number = 5;
-	 
-	  for(int i=0; i<=number; i++){
-		
-		sum = sum +(2*i);
-
-		}
-	System.out.println("Sum of even number is: ");
-	System.out.println(sum);
+        for(int i=0; i<=num; i++){
+            sum += 2 * i;
+        }
+        System.out.println("Sum of even number is :");
+        System.out.println(sum);
 	
 
 	}
